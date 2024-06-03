bfffc9d69b70e0f3a2bbb78cad5cf0373172dc16
Elgan Mathias <e_mathias@hotmail.co.uk>
2024-06-03 15:54:05 +0100

    remove requestLogger

--- backend/index.js
+++ backend/index.js
@@ -21,20 +21,11 @@ let notes = [
 
 app.use(express.static('dist'))
 
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
-
 const cors = require('cors')
 
 app.use(cors())
 
 app.use(express.json())
-app.use(requestLogger)
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
